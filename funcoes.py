4dd0f6fe6257409ea1d68b12f9c799240a01d5aa
ledev1818 <ledermanleo@gmail.com>
2026-04-26 18:07:28 -0300

    guardar_dado1

--- funcoes.py
+++ funcoes.py
@@ -5,3 +5,19 @@ def rolar_dados(n):
     for i in range(n):
         listaDados.append(random.randint(1, 6))
     return listaDados
+
+def guardar_dado(dadosRolados, dadosEstoque, dadoGuardar):
+    novosRolados = []
+    novoEstoque = []
+    for i in dadosEstoque:
+        novoEstoque.append(i)
+    novoEstoque.append(dadosRolados[dadoGuardar])
+    # cria a nova lista de estoque, adicionando os antigos e o novo dado
+
+    for i in range(len(dadosRolados)):
+        if i != dadoGuardar:
+            novosRolados.append(dadosRolados[i])
+    # cria a nova lista de dados rolados, pulando o dado a ser guardado
+
+    return [novosRolados, novoEstoque]
+
